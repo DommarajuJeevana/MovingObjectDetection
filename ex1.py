6c53f74c78a4d21138ecccff8a6c1f36e2570661
DommarajuJeevana <dommaraju.jeevana@gmail.com>
2026-06-25 18:28:23 +0530

    Moving Object Detection

--- ex1.py
+++
@@ -1,6 +0,0 @@
-import cv2
-img=cv2.imread("sample1.jpg")
-cv2.imshow("tulips",img)
-cv2.imwrite("tulips.jpg",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- ex2.py
+++
@@ -1,5 +0,0 @@
-import cv2
-img=cv2.imread("sample1.jpg")
-print(img.size)
-print(img.shape)
-print(img.dtype)
